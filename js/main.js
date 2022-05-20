c0ff5d55fc32f36c3325d5277f4df6727196551a
MiguelEquisDe <mamendez86@misena.edu.co>
2022-05-19 19:03:21 -0500

    documentacion 2

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
 const API = "https://api.github.com/users/";
 
-async function buscar(){
-    const response = await fetch(API + 'MiguelEquisDe')
-    //ahora quiero traer la info en formato json
-    const data = await response.json()
-    console.log(data)
-}
-
-//se crea un modelo
+//se crea un modelo/metodo
 const app = Vue.createApp({
     data() {
         return {
-            message: 'Cambio Correcto'
+            //message: 'Buscar Mirrey'
+            busqueda: null,
+            result: null
+        }
+    },
+    //nuevo, la palabra function ya no es necesaria ya que se
+    //usa un metodo
+    methods: {
+        async buscar() {
+            const response = await fetch(API + this.busqueda)
+            //ahora quiero traer la info en formato json
+            const data = await response.json()
+            console.log(data)
+            this.result = true
         }
     }
 })
-//montamos esta informacion en el div app
